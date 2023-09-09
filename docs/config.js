09925d5946cf2fc66268e9cc6581c11276d19a49
amay077 <okuokuoku@gmail.com>
2023-09-09 11:43:55 +0900

    up by svelte

--- docs/config.js
+++
@@ -1,7 +0,0 @@
-const CLIENT_ID = 'UBWPEWEM3L1XHICEZ0TJWOHFQ3QZ0KC3XZPHUXRRVWL044ZU';
-const REDIRECT_URI = 'https://amay077.github.io/swarm-check-ins/auth.html';
-const API_ENDPOINT = `https://swarm-check-ins-api.netlify.app/.netlify/functions`;
-
-// const CLIENT_ID = 'JXKR3OYSVKYPR4YSIYFUXUASGG2SXDIR1I2XDLIWIAMUK2ES';
-// const REDIRECT_URI = 'http://localhost:8080/auth.html';
-// const API_ENDPOINT = `http://localhost:9999/.netlify/functions`;
