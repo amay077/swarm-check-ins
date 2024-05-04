b975389713f051488cc668657c20946740cffd2c
amay077 <okuokuoku@gmail.com>
2024-05-05 00:22:26 +0900

    refactor

--- src/lib/MainContent.svelte
+++ src/lib/MainContent.svelte
@@ -1,17 +1,13 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { Config } from "../config";
+  import MastodonConnection from "./MastodonConnection.svelte";
 
   // localstorage からアクセストークンを取得する
   const accessToken = localStorage.getItem('sci_accessToken');
 
-  let mastodonServer = 'mastodon.cloud';
-  let mastodonCode = '';
   let mastodonAccessToken: string = localStorage.getItem('sci_mastodonAccessToken') ?? '';
-
   let expandedMastodon = false;
 
-  const mastodonTargets = Object.values(Config.post_targets.mastodon);
 
   const title = window.navigator?.canShare != null ? `Share...` : 'Copy to clipboard';
   let checkins: any[] = [];
@@ -109,48 +105,6 @@
             
     })();
   };  
-
-  const onConnectToMastodon = () => {
-    const settings = mastodonTargets.find(x => x.server === mastodonServer);
-    if (settings == null) {
-      console.error(`onApplyMastodonAccessToken -> settings:`, settings);
-      return;
-    }
-    const url = `https://${settings.server}/oauth/authorize?client_id=${settings.MASTODON_CLIENT_ID}&response_type=code&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=write`;
-    
-    // url を別タブで開く
-    window.open(url, '_blank');
-  };
-
-  const onApplyMastodonAccessToken = async () => {
-    console.log(`FIXME h_oku 後で消す  -> onApplyMastodonAccessToken -> mastodonCode:`, mastodonCode);
-
-    const settings = mastodonTargets.find(x => x.server === mastodonServer);
-    if (settings == null) {
-      console.error(`onApplyMastodonAccessToken -> settings:`, settings);
-      return;
-    }
-
-    const res = await fetch(`https://${settings.server}/oauth/token`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: `client_id=${settings.MASTODON_CLIENT_ID}&client_secret=${settings.MASTODON_CLIENT_SECRET}&grant_type=authorization_code&code=${mastodonCode}&redirect_uri=urn:ietf:wg:oauth:2.0:oob`,
-    });
-
-    if (!res.ok) {
-      console.error(`FIXME h_oku 後で消す  -> onApplyMastodonAccessToken -> res:`, res);
-      return;
-    }
-    
-    const resJson = await res.json();
-    localStorage.setItem('sci_mastodonAccessToken', JSON.stringify({ server: settings.server, access_token_response: resJson }));
-    mastodonAccessToken = JSON.stringify(resJson);
-
-    alert('Mastodon に接続しました。toot ボタンで投稿できます。');
-  };
-
 </script>
 
 
@@ -165,6 +119,7 @@
     <div class="d-flex flex-row gap-1 align-items-center" style="cursor: pointer;"  on:click={() => {
       expandedMastodon = !expandedMastodon;
     }}>
+      <h5 class="mb-0">Mastodon</h5>
       {#if !expandedMastodon}
       <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
         <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
@@ -174,43 +129,12 @@
         <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
       </svg>
       {/if}
-      <h5 class="mb-0">Mastodon</h5>
     </div>
     {#if expandedMastodon}
     <div class="p-2">
-      {#if (mastodonAccessToken?.length ?? 0) > 0}
-      <div class="d-flex flex-row gap-2 align-items-center">
-        <span>接続済み</span>
-        <button class="btn btn-sm btn-outline-primary" style="width: 60px;" on:click={() => {
-          localStorage.removeItem('sci_mastodonAccessToken');
-          mastodonAccessToken = '';
-        }}>切断</button>
-      </div>
-      {:else}
-      <div class="d-flex flex-column gap-1">
-        <div class="d-flex flex-column gap-1">
-          <span>1.Mastodon サーバーに接続</span>
-          <div class="d-flex flex-row gap-1">
-            <select class="form-select form-select-sm" bind:value={mastodonServer} >
-              {#each mastodonTargets as m}
-                <option value={m.server}>{m.name} (https://{m.server})</option>
-              {/each}
-            </select>
-            <button class="btn btn-sm btn-primary" style="width: 60px;" on:click={onConnectToMastodon}>接続</button>
-          </div>
-        </div>
-        <div class="d-flex flex-column gap-1">
-          <span>2.認証コードを貼り付けて設定</span>
-          <div class="d-flex flex-row gap-1">
-            <input class="form-control form-control-sm" type="text" bind:value={mastodonCode}>
-            <button class="btn btn-sm btn-primary" disabled={mastodonCode?.length <= 0} style="width: 60px;" on:click={onApplyMastodonAccessToken}>設定</button>
-          </div>
-        </div>
-      </div>
-      {/if}
+      <MastodonConnection />
     </div>
     {/if}
-
   </div>
 
 
---
+++ src/lib/MastodonConnection.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+  import { Config } from "../config";
+
+  const mastodonTargets = Object.values(Config.post_targets.mastodon);
+  let mastodonServer = 'mastodon.cloud';
+  let mastodonCode = '';
+  let mastodonAccessToken: string = localStorage.getItem('sci_mastodonAccessToken') ?? '';
+
+  const onConnectToMastodon = () => {
+    const settings = mastodonTargets.find(x => x.server === mastodonServer);
+    if (settings == null) {
+      console.error(`onApplyMastodonAccessToken -> settings:`, settings);
+      return;
+    }
+    const url = `https://${settings.server}/oauth/authorize?client_id=${settings.MASTODON_CLIENT_ID}&response_type=code&redirect_uri=urn:ietf:wg:oauth:2.0:oob&scope=write`;
+    
+    // url を別タブで開く
+    window.open(url, '_blank');
+  };
+
+  const onApplyMastodonAccessToken = async () => {
+    console.log(`onApplyMastodonAccessToken -> mastodonCode:`, mastodonCode);
+
+    const settings = mastodonTargets.find(x => x.server === mastodonServer);
+    if (settings == null) {
+      console.error(`onApplyMastodonAccessToken -> settings:`, settings);
+      return;
+    }
+
+    const res = await fetch(`https://${settings.server}/oauth/token`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `client_id=${settings.MASTODON_CLIENT_ID}&client_secret=${settings.MASTODON_CLIENT_SECRET}&grant_type=authorization_code&code=${mastodonCode}&redirect_uri=urn:ietf:wg:oauth:2.0:oob`,
+    });
+
+    if (!res.ok) {
+      console.error(`FIXME h_oku 後で消す  -> onApplyMastodonAccessToken -> res:`, res);
+      return;
+    }
+    
+    const resJson = await res.json();
+    localStorage.setItem('sci_mastodonAccessToken', JSON.stringify({ server: settings.server, access_token_response: resJson }));
+    mastodonAccessToken = JSON.stringify(resJson);
+
+    alert('Mastodon に接続しました。toot ボタンで投稿できます。');
+  };
+</script>
+
+{#if (mastodonAccessToken?.length ?? 0) > 0}
+<div class="d-flex flex-row gap-2 align-items-center">
+  <span>接続済み</span>
+  <button class="btn btn-sm btn-outline-primary" style="width: 60px;" on:click={() => {
+    localStorage.removeItem('sci_mastodonAccessToken');
+    mastodonAccessToken = '';
+  }}>切断</button>
+</div>
+{:else}
+<div class="d-flex flex-column gap-1">
+  <div class="d-flex flex-column gap-1">
+    <span>1.Mastodon サーバーに接続</span>
+    <div class="d-flex flex-row gap-1">
+      <select class="form-select form-select-sm" bind:value={mastodonServer} >
+        {#each mastodonTargets as m}
+          <option value={m.server}>{m.name} (https://{m.server})</option>
+        {/each}
+      </select>
+      <button class="btn btn-sm btn-primary" style="width: 60px;" on:click={onConnectToMastodon}>接続</button>
+    </div>
+  </div>
+  <div class="d-flex flex-column gap-1">
+    <span>2.認証コードを貼り付けて設定</span>
+    <div class="d-flex flex-row gap-1">
+      <input class="form-control form-control-sm" type="text" bind:value={mastodonCode}>
+      <button class="btn btn-sm btn-primary" disabled={mastodonCode?.length <= 0} style="width: 60px;" on:click={onApplyMastodonAccessToken}>設定</button>
+    </div>
+  </div>
+</div>
+{/if}
